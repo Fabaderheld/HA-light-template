223a1e78ccc7e7e5bc4c153bdb22857d22d4ff2f
Fabian Sasse <fabaderheld@gmail.com>
2023-04-18 14:45:34 +0200

    Updated parameters

--- New-HALightTemplate.ps1
+++ New-HALightTemplate.ps1
@@ -6,38 +6,51 @@
 .DESCRIPTION
     Generates a yml configuration for a Home Assistant light template (https://www.home-assistant.io/integrations/light.template/) for the use
     of a combination of a smart switch and a smart bulb e.g. Ikea Tradfri Bulb and Sonoff M5 Wall Switch, so the both are treated as ONE light entity
+    Advantages are the the light bulb is indepent of Home Assistant but still manageable
 .EXAMPLE
-    PS C:\> <example usage>
-    Explanation of what the example does
+    PS C:\> ./New-HALightTemplate.ps1 -SwitchID switch.test -LightID light.test -FriendlyName "Test Light" -UniqueID "test_light" -TemplateName "test_light" -TemplateFile "/tmp/test_light.yaml"
+    Creates a new template 'test_light', FriendlyName 'Test Light' containing LightEntity light.test and SwitchEntity switch.test
 .PARAMETER SwitchID
     Entity ID of of the switch
-.PARAMETER BulbID
-    Entity ID of the bulb
+.PARAMETER LightID
+    Entity ID of the light
 .PARAMETER FriendlyName
     Friendly Name in Home Assistant
 .PARAMETER UniqueID
     Unique ID for Home Assistant, required to manage entity from within HA GUI
 .PARAMETER TemplateName
-    Name for the new light template combining switch and bulb
+    Name for the new light template combining switch and light
 .PARAMETER TemplateFile
     Name for the new light template file
 .PARAMETER Brightness
-    Enables Brightness for Bulb
+    Enables Brightness for Light
 .PARAMETER Color
-    Enables Color for Bulb
+    Enables Color for Light
 .PARAMETER Temperature
-    Enables Color Temperature for Bulb
+    Enables Color Temperature for Light
+.NOTES
+    requires HA configuration split up set up, https://www.home-assistant.io/docs/configuration/splitting_configuration/
 #>
 
 
 [CmdletBinding()]
 param (
     [Parameter(Mandatory)]
+    # [ArgumentCompleter(
+    #     {
+    #         $Header = @{Authorization = "Bearer $Env:HA_Token" }
+    #         ((Invoke-WebRequest -Uri "$Env:HA_URL/api/states" -ContentType application/json -Headers $Header).content | ConvertFrom-Json | Where-Object { $_.entity_id -like "light.*" }).entity_id
+    #     }
+    # )]
     [String]
+    [ValidatePattern("(switch.).*")]
+    [Alias("SwitchEntity")]
     $SwitchID,
     [Parameter(Mandatory)]
+    [ValidatePattern("(light.).*")]
     [String]
-    $BulbID,
+    [Alias("LightEntity")]
+    $LightID,
     [Parameter(Mandatory)]
     [String]
     $FriendlyName,
@@ -81,7 +94,7 @@ $Light = @(
                     }
                 }
                 value_template = "{{ states('$SwitchID') }}"
-                level_template = "{{ state_attr('$BulbID','brightness')}}"
+                level_template = "{{ state_attr('$LightID','brightness')}}"
                 set_level      = @(
                     [ordered]@{
                         service = "switch.turn_on"
@@ -96,7 +109,7 @@ $Light = @(
                             brightness = "{{ brightness }}"
                         }
                         target  = @{
-                            entity_id = $BulbID
+                            entity_id = $LightID
                         }
                     }
                 )
@@ -106,7 +119,7 @@ $Light = @(
 )
 # If color set add to template
 if ($Color) {
-    $Light.lights.$TemplateName["color_template"] = "{{states('$BulbID.temperature_input') | int}}" # Need to fix $BulbID attribute for color
+    $Light.lights.$TemplateName["color_template"] = "{{states('$LightID.temperature_input') | int}}" # Need to fix $LightID attribute for color
     $Light.lights.$TemplateName["set_color"] = @(
         [ordered]@{
             service = "switch.turn_on"
@@ -121,7 +134,7 @@ if ($Color) {
                 rgb_color = "{{ color_temp }}"
             }
             target  = @{
-                entity_id = $BulbID
+                entity_id = $LightID
             }
         }
     )
@@ -129,7 +142,7 @@ if ($Color) {
 
 # If temperature set add to template
 if ($Temperature) {
-    $Light.lights.$TemplateName["temperature_template"] = "{{states('$BulbID.temperature_input') | int}}"# Need to fix $BulbID attribute for temperature
+    $Light.lights.$TemplateName["temperature_template"] = "{{states('$LightID.temperature_input') | int}}"# Need to fix $LightID attribute for temperature
     $Light.lights.$TemplateName["set_temperature"] = @(
         [ordered]@{
             service = "switch.turn_on"
@@ -144,7 +157,7 @@ if ($Temperature) {
                 color_temp = "{{ color_temp }}"
             }
             target  = @{
-                entity_id = $BulbID
+                entity_id = $LightID
             }
         }
     )
--- New-HALightTemplate.ps1.md
+++ New-HALightTemplate.ps1.md
@@ -16,14 +16,14 @@ Short description
 ### __AllParameterSets
 
 ```
-New-HALightTemplate.ps1 [-SwitchID] <String> [-BlubID] <String> [-TemplateName] <String> [-TemplateFile] <String> [-Brightness] [-Color] [-Temperature] [<CommonParameters>]
+New-HALightTemplate.ps1 [-SwitchID] <String> [-LightID] <String> [-FriendlyName] <String> [-UniqueID] <String> [-TemplateName] <String> [-TemplateFile] <String> [-Brightness] [-Color] [-Temperature] [<CommonParameters>]
 ```
 
 ## DESCRIPTION
 
 Generates a yml configuration for a Home Assistant light template (https://www.home-assistant.io/integrations/light.template/) for the use
 of a combination of a smart switch and a smart bulb e.g.
-Ikea Tradfri Bulb and Sonoff M5 Wall Switch
+Ikea Tradfri Bulb and Sonoff M5 Wall Switch, so the both are treated as ONE light entity
 
 
 ## EXAMPLES
@@ -31,8 +31,8 @@ Ikea Tradfri Bulb and Sonoff M5 Wall Switch
 ### Example 1: EXAMPLE 1
 
 ```
-<example usage>
-Explanation of what the example does
+./New-HALightTemplate.ps1 -SwitchID switch.test -LightID light.test -FriendlyName "Test Light" -UniqueID "test_light" -TemplateName "test_light" -TemplateFile "/tmp/test_light.yaml"
+Creates a new template 'test_light', FriendlyName 'Test Light' containing LightEntity light.test and SwitchEntity switch.test
 ```
 
 
@@ -44,27 +44,9 @@ Explanation of what the example does
 
 ## PARAMETERS
 
-### -BlubID
-
-Entity ID of the bulb
-
-```yaml
-Type: String
-Parameter Sets: (All)
-Aliases: 
-Accepted values: 
-
-Required: True (All) False (None)
-Position: 1
-Default value: 
-Accept pipeline input: False
-Accept wildcard characters: False
-DontShow: False
-```
-
 ### -Brightness
 
-Enables Brightness for Bulb
+Enables Brightness for Light
 
 ```yaml
 Type: SwitchParameter
@@ -82,7 +64,7 @@ DontShow: False
 
 ### -Color
 
-Enables Color for Bulb
+Enables Color for Light
 
 ```yaml
 Type: SwitchParameter
@@ -98,6 +80,42 @@ Accept wildcard characters: False
 DontShow: False
 ```
 
+### -FriendlyName
+
+Friendly Name in Home Assistant
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases: 
+Accepted values: 
+
+Required: True (All) False (None)
+Position: 2
+Default value: 
+Accept pipeline input: False
+Accept wildcard characters: False
+DontShow: False
+```
+
+### -LightID
+
+Entity ID of the light
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases: LightEntity
+Accepted values: 
+
+Required: True (All) False (None)
+Position: 1
+Default value: 
+Accept pipeline input: False
+Accept wildcard characters: False
+DontShow: False
+```
+
 ### -SwitchID
 
 Entity ID of of the switch
@@ -105,7 +123,7 @@ Entity ID of of the switch
 ```yaml
 Type: String
 Parameter Sets: (All)
-Aliases: 
+Aliases: SwitchEntity
 Accepted values: 
 
 Required: True (All) False (None)
@@ -118,7 +136,7 @@ DontShow: False
 
 ### -Temperature
 
-Enables Color Temperature for Bulb
+Enables Color Temperature for Light
 
 ```yaml
 Type: SwitchParameter
@@ -136,7 +154,43 @@ DontShow: False
 
 ### -TemplateFile
 
-{{ Fill TemplateFile Description }}
+Name for the new light template file
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases: 
+Accepted values: 
+
+Required: True (All) False (None)
+Position: 5
+Default value: 
+Accept pipeline input: False
+Accept wildcard characters: False
+DontShow: False
+```
+
+### -TemplateName
+
+Name for the new light template combining switch and light
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases: 
+Accepted values: 
+
+Required: True (All) False (None)
+Position: 4
+Default value: 
+Accept pipeline input: False
+Accept wildcard characters: False
+DontShow: False
+```
+
+### -UniqueID
+
+Unique ID for Home Assistant, required to manage entity from within HA GUI
 
 ```yaml
 Type: String
@@ -152,24 +206,6 @@ Accept wildcard characters: False
 DontShow: False
 ```
 
-### -TemplateName
-
-Name for the new light template combing switch and blub
-
-```yaml
-Type: String
-Parameter Sets: (All)
-Aliases: 
-Accepted values: 
-
-Required: True (All) False (None)
-Position: 2
-Default value: 
-Accept pipeline input: False
-Accept wildcard characters: False
-DontShow: False
-```
-
 
 ### CommonParameters
 
@@ -177,6 +213,7 @@ This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable
 
 ## NOTES
 
+requires HA configuration split up set up, https://www.home-assistant.io/docs/configuration/splitting_configuration/
 
 
 ## RELATED LINKS
